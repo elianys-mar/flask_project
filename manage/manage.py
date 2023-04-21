a97af57d8f0e84306264fddebf8f47e7e09552e2
elianys-mar <elianysm@buffalo.edu>
2023-04-21 14:01:29 -0400

    flask_projectwork

---
+++ manage/manage.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+	return 'Welcome!'
+
+
+@app.route('/greetings/christmas')
+def  merrychristmas():
+	return 'Merry Christmas!'
+
+@app.route('/greetings/newyear')
+def  newyear():
+        return 'Happy New Year!'
+
+
+if __name__ == "main":
+        app.run()
